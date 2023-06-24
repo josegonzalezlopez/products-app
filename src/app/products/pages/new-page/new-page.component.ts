296b99fc01f8c42fde8e6b3bc3fdfb6685f4bfb3
Jose Maria Gonzalez Lopez <josegonzalezlopez@gmail.com>
2023-06-23 23:37:23 -0300

    new product wip

--- src/app/products/pages/new-page/new-page.component.ts
+++ src/app/products/pages/new-page/new-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ProductService } from '../../services/products.service';
+import { Product } from '../../interfaces/product.interface';
 
 @Component({
   selector: 'app-new-page',
@@ -7,5 +10,29 @@ import { Component } from '@angular/core';
   ]
 })
 export class NewPageComponent {
+
+public productForm = new FormGroup({
+  id: new FormControl<string>(''),
+  name: new FormControl<string>('', {nonNullable: true}),
+  manufacturer: new FormControl<string>(''),
+  color: new FormControl<string>(''),
+  energyEfficiency: new FormControl<string>(''),
+  descripcion: new FormControl<string>(''),
+  altImage: new FormControl<string>(''),
+});
+
+constructor(private productService: ProductService){}
+
+get currentProduct(): Product{
+  const product = this.productForm.value as Product;
+
+  return product;
+}
+
+
+onSubmit(): void{
+  console.log(this.productForm.valid);
+  
+}
 
 }
